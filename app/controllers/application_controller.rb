8f25164c21578db2f30aed982d0d63f51d70fe9c
“kubota-ryotaro” <kubota-ryotaro@gmo-am.jp>
2018-07-19 14:40:31 +0900

    Japanese correspondence processing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+#  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  before_action :set_locale
+def default_url_options(options={})
+  { :locale => I18n.locale }
+end
+
+# リンクの多言語化に対応する
+def set_locale
+  I18n.locale = params[:locale] || I18n.default_locale
+end
 end
